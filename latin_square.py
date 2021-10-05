5d11ed110edee9b00bb4372b52450db1a52c1341
AndrewWigginCout <x@aol.com>
2021-10-05 06:05:35 -0700

    writes to string

--- latin_square.py
+++ latin_square.py
@@ -4,7 +4,8 @@ Created on Sun Oct  3 14:22:59 2021
 
 @author: X
 """
-N=4
+import copy
+N=5
 #def latin_square(g):
 #    for i in range(16):
 #        if g[i/4][i%4]=0:
@@ -13,13 +14,15 @@ N=4
 #                g[i/4][i%4]=e
 #                solv=latin_square(g)
 #                if
-def solved(g):
+def printg(g):
+    rv=''
     for row in g:
         for cell in row:
-            print(cell,end="")
-        print("")
-    print("")
-def test(g):
+            rv+=str(cell)
+        rv+='\n'
+    rv+='\n'
+    return rv
+def solved(g):
     for y in range(N):
         for x in range(N):
             if g[y][x]==0: return False
@@ -28,30 +31,41 @@ def test(g):
 def list_available(y,x,g):
     available = [k for k in range(1,N+1)]
     for j in range(N):
-        for i in range(N):
-            if (j,i) == (y,x): continue
-            if g[j][i] != 0: available.remove(g[j][i])
+        if j == y: continue
+        if g[j][x] != 0:
+            if g[j][x] in available: available.remove(g[j][x])
+    for i in range(N):
+        if i == x: continue
+        if g[y][i] != 0:
+            if g[y][i] in available: available.remove(g[y][i])
     return available
-
+def find_empty(g):
+    for y in range(N):
+        for x in range(N):
+            if g[y][x]==0: return y,x
+    return None
+count=0
+full=''
 def guess(g,depth=0):
-    solved(g)
-    print("depth=",depth)
-    if test(g):
-        solved(g)
-        return
-    for y in range(4):
-        for x in range(4):
-            if g[y][x]: continue
-            available = list_available(y,x,g)
-            for possible in available:
-                g[y][x] = possible
-                guess(g,depth+1)
-                g[y][x]=0
+    #rv=[]
+    #print("depth=",depth)
+    #printg(g)
+    if solved(g):
+        #printg(g)
+        global full
+        full+=printg(g)
+        
+        return 
+    y,x = find_empty(g)
+    #print(y,x)
+    available = list_available(y,x,g)
+    for possible in available:
+        g[y][x] = possible
+        guess(g,depth+1)
+        g[y][x]= 0
             
 g=[
-   [0,0,0,0],
-   [0,0,0,0],
-   [0,0,0,0],
-   [0,0,0,0]]
+   [0]*N for i in range(N)
+   ]
 
 guess(g)
